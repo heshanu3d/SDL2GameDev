4591b141d21d21bca8ac4b8b63d4a00f61e59e78
Shan HE <shan.he@nio.com>
2022-12-30 11:34:09 +0800

    add tmx_1.2.0-linux.zip
     - replace ExternalProjectAdd with wget
     - add third_lib info macro add_lib

--- cmake/dep.cmake
+++ cmake/dep.cmake
@@ -1,43 +1,43 @@
+macro(add_lib lib name url)
+	set(LIB.${lib}.name ${name})
+	set(LIB.${lib}.url ${url})
+endmacro()
+
+macro (download_lib lib)
+	if (NOT EXISTS "${CMAKE_SOURCE_DIR}/dep/${LIB.${lib}.name}")
+		execute_process(COMMAND wget ${LIB.${lib}.url}
+				WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/dep)
+	endif ()
+endmacro ()
+
+macro(unzip lib)
+	string(REGEX REPLACE ".zip" "" lib_name "${LIB.${lib}.name}")
+	if(NOT EXISTS "${CMAKE_SOURCE_DIR}/Third/${lib_name}")
+		if (NOT EXISTS "${CMAKE_SOURCE_DIR}/dep/${LIB.${lib}.name}")
+			message(STATUS "dep/${LIB.${lib}.name} is not exists, we'll download it first")
+			download_lib(${lib})
+		endif ()
+
+		execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_SOURCE_DIR}/Third)
+		execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_SOURCE_DIR}/Third/${lib_name})
+		execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${CMAKE_SOURCE_DIR}/dep/${LIB.${lib}.name}
+				WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/Third/${lib_name})
+	endif()
+	set(${lib}_dir ${CMAKE_SOURCE_DIR}/Third/${lib_name})
+endmacro()
+
 if (CMAKE_SYSTEM_NAME MATCHES "Windows")
-#	include(ExternalProject)
-
-#	ExternalProject_Add(libSDL2
-#			DOWNLOAD_DIR ${CMAKE_SOURCE_DIR}/dep_download
-#			URL https://github.com/libsdl-org/SDL/releases/download/release-2.26.1/SDL2-devel-2.26.1-mingw.zip
-#			)
-#	ExternalProject_Add(libSDL2_ttf_download
-#			DOWNLOAD_DIR ${CMAKE_SOURCE_DIR}/dep
-#			URL https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.20.1/SDL2_ttf-devel-2.20.1-mingw.zip
-#			)
-#	ExternalProject_Add(libSDL2_image_download
-#			DOWNLOAD_DIR ${CMAKE_SOURCE_DIR}/dep
-#			URL https://github.com/libsdl-org/SDL_image/releases/download/release-2.6.2/SDL2_image-devel-2.6.2-mingw.zip
-#			)
-#	ExternalProject_Add(libSDL2_mixer_download
-#			DOWNLOAD_DIR ${CMAKE_SOURCE_DIR}/dep
-#			URL https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.6.2/SDL2_mixer-devel-2.6.2-mingw.zip
-#			)
-#	ExternalProject_Add(libtmx_download
-#			DOWNLOAD_DIR ${CMAKE_SOURCE_DIR}/dep
-#			URL https://github.com/baylej/tmx/releases/download/tmx_1.2.0/tmx_1.2.0-win.zip
-#			)
-
-	macro(unzip lib target)
-		string(REGEX REPLACE ".zip" "" target_name "${target}")
-		if(NOT EXISTS "${CMAKE_SOURCE_DIR}/Third/${target_name}")
-			execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_SOURCE_DIR}/Third)
-			execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_SOURCE_DIR}/Third/${target_name})
-			execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${CMAKE_SOURCE_DIR}/dep/${target}
-					WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/Third/${target_name})
-		endif()
-		set(${lib}_dir ${CMAKE_SOURCE_DIR}/Third/${target_name})
-	endmacro()
-
-	unzip(SDL2 			SDL2-devel-2.26.1-mingw.zip)
-	unzip(SDL2_ttf 		SDL2_ttf-devel-2.20.1-mingw.zip)
-	unzip(SDL2_image	SDL2_image-devel-2.6.2-mingw.zip)
-	unzip(SDL2_mixer 	SDL2_mixer-devel-2.6.2-mingw.zip)
-	unzip(tmx 			tmx_1.2.0-win.zip)
-
-	set(tmx_dir ${CMAKE_SOURCE_DIR}/Third/tmx_1.2.0-win)
-endif()
+	add_lib(SDL2 		SDL2-devel-2.26.1-mingw.zip			https://github.com/libsdl-org/SDL/releases/download/release-2.26.1/SDL2-devel-2.26.1-mingw.zip)
+	add_lib(SDL2_ttf 	SDL2_ttf-devel-2.20.1-mingw.zip		https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.20.1/SDL2_ttf-devel-2.20.1-mingw.zip)
+	add_lib(SDL2_image	SDL2_image-devel-2.6.2-mingw.zip	https://github.com/libsdl-org/SDL_image/releases/download/release-2.6.2/SDL2_image-devel-2.6.2-mingw.zip)
+	add_lib(SDL2_mixer 	SDL2_mixer-devel-2.6.2-mingw.zip	https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.6.2/SDL2_mixer-devel-2.6.2-mingw.zip)
+	add_lib(tmx 		tmx_1.2.0-win.zip					https://github.com/baylej/tmx/releases/download/tmx_1.2.0/tmx_1.2.0-win.zip)
+	unzip(SDL2)
+	unzip(SDL2_ttf)
+	unzip(SDL2_image)
+	unzip(SDL2_mixer)
+	unzip(tmx)
+elseif (CMAKE_SYSTEM_NAME MATCHES "Linux")
+	add_lib(tmx tmx_1.2.0-linux.zip https://github.com/baylej/tmx/releases/download/tmx_1.2.0/tmx_1.2.0-linux.zip)
+	unzip(tmx)
+endif ()
